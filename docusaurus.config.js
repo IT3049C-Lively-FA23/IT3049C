e92723f1ec3468eb11f42ce1d55c1fe3a39f932c
Andrew Lively <andrew@lively.software>
2023-08-15 22:25:40 -0400

    Change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,68 +6,69 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'IT3049C - Web Game Development',
-  tagline: 'Andrew Lively',
-  url: 'https://IT3049C-Lively-FA23.github.io',
-  baseUrl: '/IT3049C/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
-  organizationName: 'IT3049C-Lively-FA23', // Usually your GitHub org/user name.
-  projectName: 'IT3049C', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  title: "IT3049C - Web Game Development",
+  tagline: "Andrew Lively",
+  url: "https://it-3049-c.vercel.app/",
+  baseUrl: "/IT3049C/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  organizationName: "IT3049C-Lively-FA23", // Usually your GitHub org/user name.
+  projectName: "IT3049C", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/reedws/IT3049C/tree/main/',
+          editUrl: "https://github.com/reedws/IT3049C/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/reedws/IT3049C/tree/main/',
+          editUrl: "https://github.com/reedws/IT3049C/tree/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
 
   plugins: [
-    '@docusaurus/theme-live-codeblock',
+    "@docusaurus/theme-live-codeblock",
     [
-      'content-docs',
+      "content-docs",
       /** @type {import('@docusaurus/plugin-content-docs').Options} */
       ({
-        id: 'syllabus',
-        path: 'content/syllabus',
-        routeBasePath: 'syllabus',
-        editUrl: 'https://github.com/reedws/IT3049C/tree/main/',
+        id: "syllabus",
+        path: "content/syllabus",
+        routeBasePath: "syllabus",
+        editUrl: "https://github.com/reedws/IT3049C/tree/main/",
         editCurrentVersion: true,
-        sidebarPath: require.resolve('./content/syllabus/sidebarsSyllabus.js'),
-        remarkPlugins: [require('mdx-mermaid')],
+        sidebarPath: require.resolve("./content/syllabus/sidebarsSyllabus.js"),
+        remarkPlugins: [require("mdx-mermaid")],
         showLastUpdateAuthor: true,
         showLastUpdateTime: true,
       }),
     ],
     [
-      'content-docs',
+      "content-docs",
       /** @type {import('@docusaurus/plugin-content-docs').Options} */
       ({
-        id: 'labs',
-        path: 'content/coursework',
-        routeBasePath: 'coursework',
-        editUrl: 'https://github.com/IT4063C/IT4063C.github.io/tree/main/',
+        id: "labs",
+        path: "content/coursework",
+        routeBasePath: "coursework",
+        editUrl: "https://github.com/IT4063C/IT4063C.github.io/tree/main/",
         editCurrentVersion: true,
-        sidebarPath: require.resolve('./content/coursework/sidebarsCoursework.js'),
-        remarkPlugins: [require('mdx-mermaid')],
+        sidebarPath: require.resolve(
+          "./content/coursework/sidebarsCoursework.js"
+        ),
+        remarkPlugins: [require("mdx-mermaid")],
         showLastUpdateAuthor: true,
         showLastUpdateTime: true,
       }),
@@ -86,7 +87,7 @@ const config = {
          * The position of the live playground, above or under the editor
          * Possible values: "top" | "bottom"
          */
-        playgroundPosition: 'bottom',
+        playgroundPosition: "bottom",
       },
       // announcementBar: {
       //   id: 'support_us',
@@ -97,29 +98,29 @@ const config = {
       //   isCloseable: false,
       // },
       navbar: {
-        title: 'IT3049C - Reed',
+        title: "IT3049C - Reed",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/javascript.svg',
+          alt: "My Site Logo",
+          src: "img/javascript.svg",
         },
         items: [
           {
-            to: '/syllabus/info',
-            label: 'Syllabus',
+            to: "/syllabus/info",
+            label: "Syllabus",
           },
           {
-            to: '/coursework',
-            label: 'Coursework',
+            to: "/coursework",
+            label: "Coursework",
           },
           {
-            to: '/blog',
-            label: 'Blog',
-            position: 'left'
+            to: "/blog",
+            label: "Blog",
+            position: "left",
           },
           {
-            to: '/docs/course-notes',
-            label: 'Course Notes',
-            position: 'left'
+            to: "/docs/course-notes",
+            label: "Course Notes",
+            position: "left",
           },
           // {
           //   href: 'https://ygilany-solution-access.herokuapp.com',
@@ -127,59 +128,59 @@ const config = {
           //   position: 'right',
           // },
           {
-            href: 'https://it3049c.github.io/students/',
-            label: 'Students',
-            position: 'right',
+            href: "https://it3049c.github.io/students/",
+            label: "Students",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Site map',
+            title: "Site map",
             items: [
               {
-                label: 'Syllabus',
-                to: '/syllabus/info',
+                label: "Syllabus",
+                to: "/syllabus/info",
               },
               {
-                label: 'Coursework',
-                to: '/coursework',
+                label: "Coursework",
+                to: "/coursework",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: '⏰ Scheduler',
-                href: 'https://outlook.office365.com/owa/calendar/OfficeHours6@mailuc.onmicrosoft.com/bookings/',
+                label: "⏰ Scheduler",
+                href: "https://outlook.office365.com/owa/calendar/OfficeHours6@mailuc.onmicrosoft.com/bookings/",
               },
               {
-                label: '✉️ Email',
-                href: 'mailto:livelyar@ucmail.uc.edu',
+                label: "✉️ Email",
+                href: "mailto:livelyar@ucmail.uc.edu",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               // {
               //   label: 'Solutions Access',
               //   href: 'https://ygilany-solution-access.herokuapp.com',
               // },
               {
-                label: 'Classwork Repo',
-                href: 'https://github.com/Ygilany/spring22-classwork',
+                label: "Classwork Repo",
+                href: "https://github.com/Ygilany/spring22-classwork",
               },
               {
-                label: 'GitHub Organization',
-                href: 'https://github.com/IT3049C/',
+                label: "GitHub Organization",
+                href: "https://github.com/IT3049C/",
               },
               {
-                label: 'GitHub Assignment',
-                href: 'https://github.com/IT3049C-Students',
+                label: "GitHub Assignment",
+                href: "https://github.com/IT3049C-Students",
               },
             ],
           },
@@ -191,8 +192,6 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
-
-
 };
 
 module.exports = config;
